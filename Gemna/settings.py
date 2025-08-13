589f3f508e2abc0924114e83ba22537264c88274
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-08-13 12:34:54 +0300

    added users module

--- Gemna/settings.py
+++ Gemna/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+AUTH_USER_MODEL = 'users.user'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
